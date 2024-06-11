9a90308ab4a6249a39f57c6c08ff2ef89d657c04
Law <andre75008@gmail.com>
2024-06-11 14:41:15 +0200

    config.php

--- config.php
+++ config.php
@@ -9,11 +9,13 @@ function connect_db() {
     try {
         $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo('je suis connecté');
         return $pdo;
     } catch (PDOException $e) {
         die("Connection failed: " . $e->getMessage());
     }
+    
 }
-
+connect_db();
 
 ?>
